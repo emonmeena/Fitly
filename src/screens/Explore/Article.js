1e1d9701dc4188d6839032cc1567e224b8c00e6c
maayami <mayank16meena@gmail.com>
2020-11-01 12:51:47 +0530

    Update

--- src/screens/Explore/Article.js
+++ src/screens/Explore/Article.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const Article = () =>{
+const Article = ({urlToImage, title}) =>{
     return (
         <View style={styles.articleStyle}>
             <View>
-                <Image style={styles.imageStyle} source={{uri: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/pu1epekisyas15jvw8e9.jpg"}} />
+                <Image style={styles.imageStyle} source={{uri: urlToImage}} />
             </View>
             <View style={styles.textContainerStyle}>
-                <Text style={styles.headingStyle}>Trump on Corona </Text>
-                <Text style={styles.descriptionStyle}>New York’s Twentyeight Health is taking the wildly telemedicine services for women’s health popularized by companies like Nurx and bringing them to a patient population that previously hadn’t had access.  The mission to provide women who are Medicaid or under…</Text>
+                <Text numberOfLines={2} style={styles.descriptionStyle}>{title}</Text>
             </View>
         </View>
     );    
@@ -22,20 +21,20 @@ const styles = StyleSheet.create({
     descriptionStyle: {
         fontSize: 15,
         color: 'grey',
+        
     },
     articleStyle: {
-        height: '72%',
+        height: 275,
         backgroundColor: 'white',
-        paddingTop: 15,
         borderColor: '#C0C0C0',
-        borderWidth: 1,
         borderRadius: 10,
-        margin: 8,
+        margin: 12,
         marginTop: 10
     },
     imageStyle: {
-        height: 200,
-        margin: 5
+        height: 175,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
     },
     textContainerStyle:{
         padding: 7
--- src/screens/Explore/Explore.js
+++ src/screens/Explore/Explore.js
@@ -1,40 +1,53 @@
-import React, {useState} from 'react';
-import { Button } from 'react-native';
-import { View, Text } from 'react-native';
-import { StyleSheet } from 'react-native';
-import newsAPI from '../../api/newsAPI';
-import Article from './Article';
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import newsAPI from "../../api/newsAPI";
+import Article from "./Article";
 
-const Explore = () =>{
+const Explore = () => {
+  const [articles, setArticles] = useState([]);
 
-    const [news, setNews] = useState([]);
+  useEffect(() => {
+    exploreAPI();
+  });
 
-    const exploreAPI = async () =>{
-        const response = await newsAPI.get('/everything', {
-            params:{
-                q: 'health',
-                sortBy: 'popularity'
-            }
-        });
-        setNews(response.data.articles);
-    };
+  const exploreAPI = async () => {
+    const response = await newsAPI.get("/everything", {
+      params: {
+        q: "fitness AND exercise AND yoga",
+        sortBy: "relavancy",
+      },
+    });
+    setArticles(response.data.articles);
+  };
 
-    return (
-        <View style={styles.randm}>
-            {/* <Button title="get news" onPress={exploreAPI} /> */}
-            {/* <Text style={styles.textStyle}> We found {news.length} articles </Text> */}
-            <Article />
-        </View>
-    );
-}
+  return (
+    <View style={styles.randm}>
+      <View>
+        <Text style={styles.headingStyle}>Collections</Text>
+      </View>
+      <View>
+        <FlatList
+          data={articles}
+          renderItem={(itemData) => (
+            <Article
+              key={itemData.index}
+              title={itemData.item.title}
+              urlToImage={itemData.item.urlToImage}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
-    },
-    randm: {
-        
-    }
+    headingStyle: {
+    fontSize: 20,
+  },
+  randm: {},
 });
 
 export default Explore;
