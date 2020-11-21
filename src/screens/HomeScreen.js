ecb3f7a7b85cbde01f105ab306b6abca4652d8a5
devanshjoshi104 <17174854d@gmail.com>
2020-11-21 13:42:14 +0530

    profile created

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from '@expo/vector-icons';
 import { Text, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ConsultDoctor from './ConsultDoctor/ConsultDoctor';
@@ -11,9 +12,40 @@ const Tab = createBottomTabNavigator();
 
 const HomeScreen = () => {
   return <NavigationContainer>
-  <Tab.Navigator>
+  <Tab.Navigator
+     screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'LifeStyle') {
+              iconName = focused ? 'md-medkit' : 'md-medkit';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'md-contact' : 'md-contact';
+            }
+               else if (route.name === 'Fitness') {
+              iconName = focused ? 'md-analytics' : 'md-analytics';
+            }
+               else if (route.name === 'Explore') {
+              iconName = focused ? 'md-paper' : 'md-paper';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+         },
+       })
+       }
+        tabBarOptions={{
+          activeTintColor: 'yellow',
+          inactiveTintColor: 'gray',
+          activeBackgroundColor:'darkblue',
+          inactiveBackgroundColor:'white',
+          tabStyle:{alignItems:'center',
+          justifyContent:'center',
+          fontWeight:1000,
+          }
+        }}>
           <Tab.Screen name="Explore" component={Explore} />
-          <Tab.Screen name="Consult" component={ConsultDoctor} />
+          <Tab.Screen name="LifeStyle" component={ConsultDoctor} />
           <Tab.Screen name="Fitness" component={FitnessTracker} />
           <Tab.Screen name="Profile" component={Profile} />
          </Tab.Navigator>
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,14 +1,94 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native'
+import React ,{ Component } from 'react';
+import { Text, View,StyleSheet,Image,Button   } from 'react-native'
 
-const Profile = () =>{
-    return <Text style={styles.textStyle}>Profile</Text>;
-}
+
+
+class Profile extends Component {
+	  render() {
+ return (
+   <View style={styles.textStyle}>
+   <Image
+          source={{
+            uri: 'https://media.npr.org/assets/img/2020/05/05/gettyimages-693140990_custom-96572767b03e0e649349fdb6d38d649e6ccaed75-s800-c85.jpg',
+          }} style={styles.Coverpic}/>
+    
+
+     <Image
+          source={{
+            uri: 'https://media.npr.org/assets/img/2020/05/05/gettyimages-693140990_custom-96572767b03e0e649349fdb6d38d649e6ccaed75-s800-c85.jpg',
+          }} style={styles.Profilpic}/>
+          <View style={{height:'45%',justifyContent:'space-around',alignItems:'center'}}>
+
+          <Text style={{fontSize:30,
+            borderBottomColor: "red",
+              borderBottomWidth: StyleSheet.hairlineWidth}}>Tom Cruise</Text>
+
+
+          <Text style={{fontSize:13,
+          borderBottomWidth: StyleSheet.hairlineWidth, borderBottomColor: "red",}}>devansh_j@cs.iitr.ac.in</Text>
+        
+          <Text style={{fontSize:15,
+            
+           borderBottomWidth: StyleSheet.hairlineWidth,
+             borderBottomColor: "red",}} >AGE:10</Text  >
+
+
+             
+          <Text style={{fontSize:20,borderRadius:20,
+          borderColor:'black',
+          borderWidth:4,
+          padding:5
+   
+          }} >Fitly Score:10</Text  >
+          <View>
+          <Button
+            onPress={''}
+            title="Edit Profile"
+          />
+          </View>
+       
+
+      
+          </View>
+ </View>);
+
+}}
+
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        flex:1,
+      
+        fontSize: 30,
+        alignItems:"center",
+        justifyContent:"center",
+        color:'white',
+
+        backgroundColor:'white',
+        // height:'100%'
+    },
+    Profilpic:{
+       backgroundColor:'gray',
+       height:150,
+       width:150,
+       borderRadius:75,
+       borderColor:'black',
+         borderWidth: 4,
+    borderColor: "white",
+       marginTop:100
+
+    },
+    Coverpic:{
+       backgroundColor:'gray',
+       height:'35%',
+       width:'100%',
+       position:'absolute',
+       top:0
+     
+      //  marginBottom:200
+
     }
 });
+
 
 export default Profile;
